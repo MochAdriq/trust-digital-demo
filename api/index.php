3a39dce15d37247badafad9018dcbd05377d37a6
Moch Adriq <mochfadillah1208@gmail.com>
2026-06-18 03:05:51 +0700

    fix: redirect bootstrap cache to /tmp

--- api/index.php
+++ api/index.php
@@ -13,18 +13,27 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Vercel Serverless: Remap storage to /tmp because Vercel is read-only
+// Vercel Serverless: Remap storage and cache to /tmp because Vercel is read-only
 $storagePath = '/tmp/storage';
 if (!is_dir($storagePath)) {
     mkdir($storagePath.'/framework/views', 0777, true);
     mkdir($storagePath.'/framework/cache/data', 0777, true);
     mkdir($storagePath.'/framework/sessions', 0777, true);
     mkdir($storagePath.'/logs', 0777, true);
+    mkdir($storagePath.'/bootstrap/cache', 0777, true);
 }
+
+// Redirect cache files to /tmp
+$_ENV['APP_SERVICES_CACHE'] = $storagePath.'/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $storagePath.'/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = $storagePath.'/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $storagePath.'/bootstrap/cache/routes-v7.php';
+$_ENV['APP_EVENTS_CACHE'] = $storagePath.'/bootstrap/cache/events.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
 $app->useStoragePath($storagePath);
 
 try {
